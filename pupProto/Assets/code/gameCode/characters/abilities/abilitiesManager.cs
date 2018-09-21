3c337c457153b5f32e59a3d2b5ca85431bcc4d0b
kyle <kylengelmann@gmail.com>
2018-09-20 21:05:32 -0500

    Started checkpoint stuff, stopped move while animating physics

--- pupProto/Assets/code/gameCode/characters/abilities/abilitiesManager.cs
+++ pupProto/Assets/code/gameCode/characters/abilities/abilitiesManager.cs
@@ -6,6 +6,8 @@
 		this.events = events;
 		events.dash.onDashStart += onDashStart;
 		events.dash.onDashEnd += onDashEnd;
+        events.physicsAnimation.onEnable += onPhysicsAnimate;
+        events.physicsAnimation.onDisable += onStopPhysicsAnimate;
 	}
 	
 	void onDashStart()
@@ -21,4 +23,20 @@
 		events.jump.setActive.Invoke(true);
 		events.wall.setActive.Invoke(true);
 	}
+
+    void onPhysicsAnimate()
+    {
+        events.move.setActive.Invoke(false);
+        events.jump.setActive.Invoke(false);
+        events.wall.setActive.Invoke(false);
+        events.dash.setAcive.Invoke(false);
+    }
+
+    void onStopPhysicsAnimate()
+    {
+        events.move.setActive.Invoke(true);
+        events.jump.setActive.Invoke(true);
+        events.wall.setActive.Invoke(true);
+        events.dash.setAcive.Invoke(true);
+    }
 }
